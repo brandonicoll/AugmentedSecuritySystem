bf956d7f480757a80232f545a70317453bc1a39c
Yuvraj Sandhu <n01313276@humberr.ca>
2021-02-28 01:13:07 -0500

    connected to firebase sdk

---
+++ app/src/main/java/augmented/security/asecuritysystem/firebase/userdata.java
@@ -0,0 +1,33 @@
+package augmented.security.asecuritysystem.firebase;
+
+public class userdata {
+
+    public String range;
+    public Integer timestamp;
+
+
+
+    public userdata(String range,Integer timestamp)
+    {
+        this.range = range;
+        this.timestamp = timestamp;
+
+
+    }
+
+    public String getRange() {
+        return range;
+    }
+
+    public Integer getTimestamp() {
+        return timestamp;
+    }
+
+    public void setRange(String range) {
+        this.range = range;
+    }
+
+    public void setTimestamp(Integer timestamp) {
+        this.timestamp = timestamp;
+    }
+}
