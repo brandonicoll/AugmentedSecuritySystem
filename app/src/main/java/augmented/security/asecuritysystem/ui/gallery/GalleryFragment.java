ab5f9433e7fe4dd809b2b15e93b07a1271172556
Yuvraj Sandhu <n01313276@humberr.ca>
2021-03-02 17:55:47 -0500

    this fragment reads distance data from the firebase now.

--- app/src/main/java/augmented/security/asecuritysystem/ui/gallery/GalleryFragment.java
+++ app/src/main/java/augmented/security/asecuritysystem/ui/gallery/GalleryFragment.java
@@ -8,10 +8,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,6 +21,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import augmented.security.asecuritysystem.R;
+import augmented.security.asecuritysystem.firebase.distance;
 import augmented.security.asecuritysystem.firebase.userdata;
 
 public class GalleryFragment extends Fragment {
@@ -37,7 +40,51 @@ public class GalleryFragment extends Fragment {
         reference = FirebaseDatabase.getInstance().getReference("distance");
 
 
-        reference.child("-MUjfQvJr9V4tXsTmwQd").addListenerForSingleValueEvent(new ValueEventListener() {
+        reference.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                distance distanceprofile = snapshot.getValue(distance.class);
+
+                long range = distanceprofile.range;
+                int time = distanceprofile.timestamp;
+
+                textView.setText(String.format("%s%s", "Range: ", range));
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                distance distanceprofile = snapshot.getValue(distance.class);
+
+                long range = distanceprofile.range;
+                int time = distanceprofile.timestamp;
+
+                textView.setText(String.format("%s%s", "Range: ", range));
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                distance distanceprofile = snapshot.getValue(distance.class);
+
+                long range = distanceprofile.range;
+                int time = distanceprofile.timestamp;
+
+                textView.setText(String.format("%s%s", "Range: ", range));
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //Hardcoded Child, no longer needed.
+
+      /*  reference.child("-MUjfQvJr9V4tXsTmwQd").addListenerForSingleValueEvent(new ValueEventListener() {
             //Sets the textview for the greeting message
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
@@ -56,7 +103,7 @@ public class GalleryFragment extends Fragment {
             public void onCancelled(@NonNull DatabaseError error) {
                 Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_LONG).show();
             }
-        });
+        });*/
 
         return root;
     }
