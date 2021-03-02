18af35dd4514fc329dd12327ec19b3e7502578be
Yuvraj Sandhu <n01313276@humberr.ca>
2021-03-02 17:46:09 -0500

    modified distance data class to correct

--- app/src/main/java/augmented/security/asecuritysystem/firebase/distance.java
+++ app/src/main/java/augmented/security/asecuritysystem/firebase/distance.java
@@ -1,39 +1,19 @@
 package augmented.security.asecuritysystem.firebase;
 
 public class distance {
-    public String range;
+    public long range;
     public Integer timestamp;
 
-    public distance(){
+    public distance() {
 
     }
 
-    public distance(String range, Integer timestamp){
+
+    public distance(long range,Integer timestamp)
+    {
         this.range = range;
         this.timestamp = timestamp;
-    }
 
-    public String getRange() {
-        return range;
-    }
 
-    public Integer getTimestamp() {
-        return timestamp;
-    }
-
-    public void setTimestamp(Integer timestamp) {
-        this.timestamp = timestamp;
-    }
-
-    public void setRange(String range) {
-        this.range = range;
-    }
-
-    @Override
-    public String toString() {
-        return "distance{" +
-                "range='" + range + '\'' +
-                ", timestamp=" + timestamp +
-                '}';
     }
 }
