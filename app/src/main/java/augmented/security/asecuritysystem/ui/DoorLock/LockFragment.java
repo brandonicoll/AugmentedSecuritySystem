375fc2e0f923ecf301f546bf701909db4764c24b
FernandodoPrado <n01329973@humbermail.ca>
2021-03-02 12:26:13 -0500

    start of lock reading

--- app/src/main/java/augmented/security/asecuritysystem/ui/DoorLock/LockFragment.java
+++ app/src/main/java/augmented/security/asecuritysystem/ui/DoorLock/LockFragment.java
@@ -5,31 +5,58 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import augmented.security.asecuritysystem.R;
+import augmented.security.asecuritysystem.firebase.userdata;
 
 public class LockFragment extends Fragment {
 
-    private LockViewModel lockViewModel;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+    private String userID;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        lockViewModel =
-                new ViewModelProvider(this).get(LockViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        lockViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        TextView textView = root.findViewById(R.id.text_home);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("distance");
+
+
+        reference.child("-MUjfQvJr9V4tXsTmwQd").addListenerForSingleValueEvent(new ValueEventListener() {
+            //Sets the textview for the greeting message
             @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+
+                userdata userprofile = snapshot.getValue(userdata.class);
+
+                long range = userprofile.range;
+                int time = userprofile.timestamp;
+
+                textView.setText(String.format("%s%s", "Range: ", range));
+
+            }
+            //Displays an error message if you cancel the onCreate
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_LONG).show();
             }
         });
+
         return root;
     }
-}
+    }
--- app/src/main/java/augmented/security/asecuritysystem/ui/gallery/GalleryFragment.java
+++ app/src/main/java/augmented/security/asecuritysystem/ui/gallery/GalleryFragment.java
@@ -8,10 +8,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
