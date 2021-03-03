edd29e43a62626e783611f05c6300c42310562cb
Yuvraj Sandhu <n01313276@humberr.ca>
2021-03-02 19:08:18 -0500

    retrives lock door data from databass, fixed crashes.

--- app/src/main/java/augmented/security/asecuritysystem/ui/DoorLock/LockFragment.java
+++ app/src/main/java/augmented/security/asecuritysystem/ui/DoorLock/LockFragment.java
@@ -8,10 +8,12 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,45 +21,71 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import augmented.security.asecuritysystem.R;
+import augmented.security.asecuritysystem.firebase.distance;
+import augmented.security.asecuritysystem.firebase.rfid;
 import augmented.security.asecuritysystem.firebase.userdata;
 
 public class LockFragment extends Fragment {
 
     private FirebaseUser user;
-    private DatabaseReference reference;
+    private DatabaseReference breference;
     private String userID;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        View root = inflater.inflate(R.layout.fragment_home, container, false);
         TextView textView = root.findViewById(R.id.text_home);
 
-        //Causes app to crash
-
-      /*  user = FirebaseAuth.getInstance().getCurrentUser();
-        reference = FirebaseDatabase.getInstance().getReference("distance");
 
 
-        reference.child("-MUjfQvJr9V4tXsTmwQd").addListenerForSingleValueEvent(new ValueEventListener() {
-            //Sets the textview for the greeting message
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        breference = FirebaseDatabase.getInstance().getReference("rfid");
+
+
+        breference.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                rfid rfidprofile = snapshot.getValue(rfid.class);
 
+                long id = rfidprofile.id;
+                String info = rfidprofile.info;
+                Integer time = rfidprofile.timestamp;
 
-                userdata userprofile = snapshot.getValue(userdata.class);
+                textView.setText(String.format("%s%s", "Id: ", id) + "\n" + String.format("%s%s", "Info: ", info));
+            }
 
-                long range = userprofile.range;
-                int time = userprofile.timestamp;
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                rfid rfidprofile = snapshot.getValue(rfid.class);
 
-                textView.setText(String.format("%s%s", "Range: ", range));
+                long id = rfidprofile.id;
+                String info = rfidprofile.info;
+                Integer time = rfidprofile.timestamp;
+
+                textView.setText(String.format("%s%s", "Id: ", id) + "\n" + String.format("%s%s", "Info: ", info));
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
 
             }
-            //Displays an error message if you cancel the onCreate
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                rfid rfidprofile = snapshot.getValue(rfid.class);
+
+                long id = rfidprofile.id;
+                String info = rfidprofile.info;
+                Integer time = rfidprofile.timestamp;
+
+                textView.setText(String.format("%s%s", "Id: ", id) + "\n" + String.format("%s%s", "Info: ", info));
+            }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_LONG).show();
+
             }
-        });*/
+        });
 
         return root;
     }
