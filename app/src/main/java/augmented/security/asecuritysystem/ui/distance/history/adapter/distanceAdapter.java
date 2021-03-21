d8571f8877ba40488cff3261b6aafa3f5eb3782a
Yuvraj Sandhu <n01313276@humberr.ca>
2021-03-21 02:01:13 -0400

    code format cleanup

--- app/src/main/java/augmented/security/asecuritysystem/ui/distance/history/adapter/distanceAdapter.java
+++ app/src/main/java/augmented/security/asecuritysystem/ui/distance/history/adapter/distanceAdapter.java
@@ -13,20 +13,14 @@ import augmented.security.asecuritysystem.R;
 import augmented.security.asecuritysystem.firebase.distance;
 
 
-public class distanceAdapter extends FirebaseRecyclerAdapter<
-        distance, distanceAdapter.distanceViewholder> {
+public class distanceAdapter extends FirebaseRecyclerAdapter<distance, distanceAdapter.distanceViewholder> {
 
-    public distanceAdapter(
-            @NonNull FirebaseRecyclerOptions<distance> options)
-    {
+    public distanceAdapter(@NonNull FirebaseRecyclerOptions<distance> options) {
         super(options);
     }
 
     @Override
-    protected void
-    onBindViewHolder(@NonNull distanceViewholder holder,
-                     int position, @NonNull distance model)
-    {
+    protected void onBindViewHolder(@NonNull distanceViewholder holder, int position, @NonNull distance model) {
 
         holder.range.setText(String.valueOf(model.getRange()));
         holder.time.setText(String.valueOf(model.getDate(model.getTimestamp())));
@@ -36,18 +30,14 @@ public class distanceAdapter extends FirebaseRecyclerAdapter<
 
     @NonNull
     @Override
-    public distanceViewholder
-    onCreateViewHolder(@NonNull ViewGroup parent,
-                       int viewType)
-    {
+    public distanceViewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view
                 = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.distance_item, parent, false);
         return new distanceAdapter.distanceViewholder(view);
     }
 
-    public static class distanceViewholder
-            extends RecyclerView.ViewHolder {
+    public static class distanceViewholder extends RecyclerView.ViewHolder {
         TextView range,time;
         public distanceViewholder(@NonNull View itemView)
         {
