240971e62a448fe446ab4cfeafebf7cdf48362c9
Yuvraj Sandhu <n01313276@humberr.ca>
2021-04-19 17:11:01 -0400

    fire firebase cloud messaging configured

--- app/src/main/java/augmented/security/asecuritysystem/ui/fire/FireFragment.java
+++ app/src/main/java/augmented/security/asecuritysystem/ui/fire/FireFragment.java
@@ -5,6 +5,7 @@ import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +20,8 @@ import androidx.core.app.NotificationCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -26,6 +29,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.messaging.FirebaseMessaging;
 
 import augmented.security.asecuritysystem.R;
 import augmented.security.asecuritysystem.firebase.data;
@@ -35,6 +39,7 @@ public class FireFragment extends Fragment {
     private FireViewModel slideshowViewModel;
     private FirebaseUser user;
     private DatabaseReference reference;
+    private static final String TAG = "MainActivity";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -49,106 +54,77 @@ public class FireFragment extends Fragment {
         user = FirebaseAuth.getInstance().getCurrentUser();
         reference = FirebaseDatabase.getInstance().getReference("data");
 
-        //notification channel and manager code
-        int notifyID = 1;
-        String CHANNEL_ID = "my_channel_01";
-        CharSequence name = "fire";
-        int importance = NotificationManager.IMPORTANCE_HIGH;
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(getActivity(),CHANNEL_ID)
-                .setContentTitle("ALERT:")
-                .setContentText("Your fire alarm has detected a fire!")
-                .setSmallIcon(R.drawable.fire_png_transparent)
-                .setChannelId(CHANNEL_ID)
-                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-                .setAutoCancel(true);
-
-        NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, name, importance);
-
-
         reference.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 data dataprofile = snapshot.getValue(data.class);
-
                 String fire = dataprofile.fire;
                 Integer time = dataprofile.timestamp;
-
                 textView.setText(String.format("%s", fire));
                 if (fire.equals("We're safe! No fire.")) {
                     fireimg.setVisibility(View.INVISIBLE);
                     textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 25);
                 }
                 else  {
-                    if(getActivity() !=null) {
-                        fireimg.setVisibility(View.VISIBLE);
-                        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 36);
-                        NotificationManager mNotificationManager = (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
-                        mChannel.setImportance(NotificationManager.IMPORTANCE_HIGH);
-                        mNotificationManager.createNotificationChannel(mChannel);
-                        mNotificationManager.notify(notifyID, builder.build());
-                    }
+                    fireimg.setVisibility(View.VISIBLE);
+                    textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 36);
+
                 }
             }
-
             @Override
             public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 data dataprofile = snapshot.getValue(data.class);
-
                 String fire = dataprofile.fire;
                 Integer time = dataprofile.timestamp;
-
                 textView.setText(String.format("%s", fire));
                 if (fire.equals("We're safe! No fire.")) {
                     fireimg.setVisibility(View.INVISIBLE);
                 }
                 else  {
-                    if(getActivity() !=null) {
-                        fireimg.setVisibility(View.VISIBLE);
-                        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 36);
-                        NotificationManager mNotificationManager = (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
-                        mChannel.setImportance(NotificationManager.IMPORTANCE_HIGH);
-                        mNotificationManager.createNotificationChannel(mChannel);
-                        mNotificationManager.notify(notifyID, builder.build());
-                    }
+                    fireimg.setVisibility(View.VISIBLE);
+
                 }
             }
-
             @Override
             public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-
             }
-
             @Override
             public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
                 data dataprofile = snapshot.getValue(data.class);
-
                 String fire = dataprofile.fire;
                 Integer time = dataprofile.timestamp;
-
                 textView.setText(String.format("%s", fire));
                 if (fire.equals("We're safe! No fire.")) {
                     fireimg.setVisibility(View.INVISIBLE);
                 }
                 else  {
-                    if(getActivity() !=null) {
-                        fireimg.setVisibility(View.VISIBLE);
-                        textView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 36);
-                        NotificationManager mNotificationManager = (NotificationManager) getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
-                        mChannel.setImportance(NotificationManager.IMPORTANCE_HIGH);
-                        mNotificationManager.createNotificationChannel(mChannel);
-                        mNotificationManager.notify(notifyID, builder.build());
-                    }
+                    fireimg.setVisibility(View.VISIBLE);
+                 
                 }
             }
-
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
                 Toast.makeText(getContext(), "Something went wrong", Toast.LENGTH_LONG).show();
             }
         });
-
-
+        createTopic();
         return root;
+    }
+    public void createTopic() {
+        try {
+            FirebaseMessaging.getInstance().subscribeToTopic("fire")
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            String msg = getString(R.string.msg_subscribed);
+                            if (!task.isSuccessful()) {
+                                msg = getString(R.string.msg_subscribe_failed);
+                            }
+                            Log.d(TAG, msg);
+                        }
+                    });
+        }catch (NullPointerException ex){
+            ex.printStackTrace();
+        }
     }
 }
